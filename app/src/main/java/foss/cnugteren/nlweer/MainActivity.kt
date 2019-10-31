d40761329c4190110e3e20a3803738c4833633b2
Cedric Nugteren <web@cedricnugteren.nl>
2019-10-31 22:41:52 +0100

    Removed snackbar and refresh button

--- app/src/main/java/foss/cnugteren/nlweer/MainActivity.kt
+++ app/src/main/java/foss/cnugteren/nlweer/MainActivity.kt
@@ -27,11 +27,6 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Reloading page [TODO]", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
