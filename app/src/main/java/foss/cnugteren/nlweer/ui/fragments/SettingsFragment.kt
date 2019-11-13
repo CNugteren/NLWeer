d4a1e02e7eb2187b9eca7837679afed7d203280d
Cedric Nugteren <web@cedricnugteren.nl>
2019-11-13 20:09:25 +0100

    Made the current lon/lat values appear as subtitles in the settings menu

--- app/src/main/java/foss/cnugteren/nlweer/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/foss/cnugteren/nlweer/ui/fragments/SettingsFragment.kt
@@ -6,6 +6,7 @@ import foss.cnugteren.nlweer.R
 import android.content.SharedPreferences
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 
 
 class SettingsFragment : PreferenceFragmentCompat(),
@@ -13,8 +14,28 @@ class SettingsFragment : PreferenceFragmentCompat(),
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        // Sets the initial values as summaries
+        val latString = sharedPreferences.getString("location_latitude", null)
+        val lonString = sharedPreferences.getString("location_longitude", null)
+        findPreference<Preference>("location_latitude")?.summary = latString
+        findPreference<Preference>("location_longitude")?.summary = lonString
     }
 
+    override fun onResume() {
+        super.onResume()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    // If the value of the settings change, sets the new values as summaries
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         val pref = findPreference<Preference>(key)
         if (pref is EditTextPreference) {
