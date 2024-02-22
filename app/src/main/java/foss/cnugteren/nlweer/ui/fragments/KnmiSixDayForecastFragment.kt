fe220d1ced817803b26f6529c8dfa79d1fcf6837
Leonard <leonard@posteo.net>
2024-02-22 14:42:36 +0100

    Split table into multiple 'rows'

--- app/src/main/java/foss/cnugteren/nlweer/ui/fragments/KnmiSixDayForecastFragment.kt
+++ app/src/main/java/foss/cnugteren/nlweer/ui/fragments/KnmiSixDayForecastFragment.kt
@@ -82,6 +82,10 @@ class KnmiSixDayForecastFragment : Fragment() {
                           font-weight: bold;
                           color:""".trimIndent() + textColor + """;                          
                         }
+                        tr:nth-child(19) {
+                          font-weight: bold;
+                          color:""".trimIndent() + textColor + """;                          
+                        }
                         tr:nth-child(2n) {
                           color: grey;
                         }
@@ -99,30 +103,18 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>Wo</td>
                         <td>Do</td>
                         <td>Vr</td>
-                        <td>Za</td>
-                        <td>Zo</td>
-                        <td>Ma</td>
                       </tr>
                       <tr>
                         <td>21-02-2024</td>
                         <td>22-02-2024</td>
                         <td>23-02-2024</td>
-                        <td>24-02-2024</td>
-                        <td>25-02-2024</td>
-                        <td>26-02-2024</td>
                       </tr>
                       <tr>
                         <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-0ff4b4be825bd046a86a22357884275f87438050fd9793d6ee9180cdc70a25e1.svg" width="60px"/></td>
                         <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/Regen-5cdc504cb0cc00fd9f218c42b88ea0b3ba00d26240de3d4aeacef0f37d5e66ab.svg" width="60px"/></td>
                         <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-0ff4b4be825bd046a86a22357884275f87438050fd9793d6ee9180cdc70a25e1.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
                       </tr>
                       <tr>
-                        <td>Max.</td>
-                        <td>Max.</td>
-                        <td>Max.</td>
                         <td>Max.</td>
                         <td>Max.</td>
                         <td>Max.</td>
@@ -131,14 +123,8 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>10°</td>
                         <td>13/15°</td>
                         <td>9/10°</td>
-                        <td>8/9°</td>
-                        <td>8/10°</td>
-                        <td>8/11°</td>
                       </tr>
                       <tr>
-                        <td>Min.</td>
-                        <td>Min.</td>
-                        <td>Min.</td>
                         <td>Min.</td>
                         <td>Min.</td>
                         <td>Min.</td>
@@ -147,14 +133,8 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>8°</td>
                         <td>8/9°</td>
                         <td>3/4°</td>
-                        <td>2/3°</td>
-                        <td>1/2°</td>
-                        <td>1/5°</td>
                       </tr>
                       <tr>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
                         <td>Neerslag</td>
                         <td>Neerslag</td>
                         <td>Neerslag</td>
@@ -163,14 +143,8 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>5/10mm</td>
                         <td>10/20mm</td>
                         <td>1/5mm</td>
-                        <td>0/3mm</td>
-                        <td>0/5mm</td>
-                        <td>0/5mm</td>
                       </tr>
                       <tr>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
                         <td>Neerslagkans</td>
                         <td>Neerslagkans</td>
                         <td>Neerslagkans</td>
@@ -179,14 +153,8 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>90%</td>
                         <td>100%</td>
                         <td>70%</td>
-                        <td>50%</td>
-                        <td>60%</td>
-                        <td>60%</td>
                       </tr>
                       <tr>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
                         <td>Zonneschijn</td>
                         <td>Zonneschijn</td>
                         <td>Zonneschijn</td>
@@ -195,14 +163,8 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>10%</td>
                         <td>10%</td>
                         <td>30%</td>
-                        <td>20%</td>
-                        <td>30%</td>
-                        <td>30%</td>
                       </tr>
                       <tr>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
                         <td>Windkracht</td>
                         <td>Windkracht</td>
                         <td>Windkracht</td>
@@ -211,10 +173,107 @@ class KnmiSixDayForecastFragment : Fragment() {
                         <td>Z 4</td>
                         <td>Z 5</td>
                         <td>ZW 5</td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>                                            
+                      <tr>
+                        <td>Za</td>
+                        <td>Zo</td>
+                        <td>Ma</td>
+                      </tr>
+                      <tr>
+                        <td>24-02-2024</td>
+                        <td>25-02-2024</td>
+                        <td>26-02-2024</td>
+                      </tr>
+                      <tr>
+                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-0ff4b4be825bd046a86a22357884275f87438050fd9793d6ee9180cdc70a25e1.svg" width="60px"/></td>
+                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
+                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
+                      </tr>
+                      <tr>
+                        <td>Max.</td>
+                        <td>Max.</td>
+                        <td>Max.</td>
+                      </tr>
+                      <tr>
+                        <td>8/9°</td>
+                        <td>8/10°</td>
+                        <td>8/11°</td>
+                      </tr>
+                      <tr>
+                        <td>Min.</td>
+                        <td>Min.</td>
+                        <td>Min.</td>
+                      </tr>                      
+                      <tr>
+                        <td>2/3°</td>
+                        <td>1/2°</td>
+                        <td>1/5°</td>
+                      </tr>
+                      <tr>
+                        <td>Neerslag</td>
+                        <td>Neerslag</td>
+                        <td>Neerslag</td>
+                      </tr>                      
+                      <tr>
+                        <td>0/3mm</td>
+                        <td>0/5mm</td>
+                        <td>0/5mm</td>
+                      </tr>
+                      <tr>
+                        <td>Neerslagkans</td>
+                        <td>Neerslagkans</td>
+                        <td>Neerslagkans</td>
+                      </tr>                                            
+                      <tr>
+                        <td>50%</td>
+                        <td>60%</td>
+                        <td>60%</td>
+                      </tr>
+                      <tr>
+                        <td>Zonneschijn</td>
+                        <td>Zonneschijn</td>
+                        <td>Zonneschijn</td>
+                      </tr>                      
+                      <tr>
+                        <td>20%</td>
+                        <td>30%</td>
+                        <td>30%</td>
+                      </tr>
+                      <tr>
+                        <td>Windkracht</td>
+                        <td>Windkracht</td>
+                        <td>Windkracht</td>
+                      </tr>
+                      <tr>
                         <td>ZW 4</td>
                         <td>Z 4</td>
                         <td>NO 3</td>
                       </tr>
+
                     </table>
                     <p>""".trimIndent() + getString(R.string.menu_knmi_text_source) + """</p>
                 </body>
