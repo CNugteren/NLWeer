335d968ca9d9c24f004f8e4525e515e7cd98a1ff
Leonard <leonard@posteo.net>
2024-02-24 10:24:35 +0100

    WIP

--- app/src/main/java/foss/cnugteren/nlweer/ui/fragments/KnmiSixDayForecastFragment.kt
+++ app/src/main/java/foss/cnugteren/nlweer/ui/fragments/KnmiSixDayForecastFragment.kt
@@ -18,7 +18,7 @@ import foss.cnugteren.nlweer.databinding.FragmentKnmiSixdayforecastBinding
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.jsoup.nodes.TextNode
+import kotlin.math.ceil
 import kotlin.math.floor
 
 class KnmiSixDayForecastFragment : Fragment() {
@@ -80,226 +80,6 @@ class KnmiSixDayForecastFragment : Fragment() {
             textColor = "rgb(193, 193, 193)" // Android dark mode color
         }
 
-        val columnsPerRow = CalculateColumnsPerRow(webView)
-
-        webView.loadData(
-            """
-            <html>
-                <head>
-                    <style type='text/css'>
-                        body {
-                          font-size: 14px;
-                          color:""".trimIndent() + textColor + """;
-                          background-color:""".trimIndent() + backgroundColor + """;
-                        }
-                        tr:nth-child(2n+3) {
-                          height: 30px;
-                          vertical-align: text-top;
-                          color:""".trimIndent() + textColor + """;
-                        }
-                        tr:nth-child(1) {
-                          font-weight: bold;
-                          color:""".trimIndent() + textColor + """;                          
-                        }
-                        tr:nth-child(19) {
-                          font-weight: bold;
-                          color:""".trimIndent() + textColor + """;                          
-                        }
-                        tr:nth-child(2n) {
-                          color: grey;
-                        }
-                        tr {
-                          font-size: 14px;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <table>
-                      <colgroup>
-                        <col style="min-width:""".trimIndent() + columnWidth + """px" span="6" />
-                      </colgroup>
-                      <tr>
-                        <td>Wo</td>
-                        <td>Do</td>
-                        <td>Vr</td>
-                      </tr>
-                      <tr>
-                        <td>21-02-2024</td>
-                        <td>22-02-2024</td>
-                        <td>23-02-2024</td>
-                      </tr>
-                      <tr>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-0ff4b4be825bd046a86a22357884275f87438050fd9793d6ee9180cdc70a25e1.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/Regen-5cdc504cb0cc00fd9f218c42b88ea0b3ba00d26240de3d4aeacef0f37d5e66ab.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
-                      </tr>
-                      <tr>
-                        <td>Max.</td>
-                        <td>Max.</td>
-                        <td>Max.</td>
-                      </tr>
-                      <tr>
-                        <td>10°</td>
-                        <td>13/15°</td>
-                        <td>9/10°</td>
-                      </tr>
-                      <tr>
-                        <td>Min.</td>
-                        <td>Min.</td>
-                        <td>Min.</td>
-                      </tr>                      
-                      <tr>
-                        <td>8°</td>
-                        <td>8/9°</td>
-                        <td>3/4°</td>
-                      </tr>
-                      <tr>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
-                      </tr>                      
-                      <tr>
-                        <td>5/10mm</td>
-                        <td>10/20mm</td>
-                        <td>1/5mm</td>
-                      </tr>
-                      <tr>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
-                      </tr>                                            
-                      <tr>
-                        <td>90%</td>
-                        <td>100%</td>
-                        <td>70%</td>
-                      </tr>
-                      <tr>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
-                      </tr>                      
-                      <tr>
-                        <td>10%</td>
-                        <td>10%</td>
-                        <td>30%</td>
-                      </tr>
-                      <tr>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
-                      </tr>
-                      <tr>
-                        <td>Z 4</td>
-                        <td>Z 5</td>
-                        <td>ZW 5</td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                      </tr>                                            
-                      <tr>
-                        <td>Za</td>
-                        <td>Zo</td>
-                        <td>Ma</td>
-                      </tr>
-                      <tr>
-                        <td>24-02-2024</td>
-                        <td>25-02-2024</td>
-                        <td>26-02-2024</td>
-                      </tr>
-                      <tr>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-0ff4b4be825bd046a86a22357884275f87438050fd9793d6ee9180cdc70a25e1.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
-                        <td><img alt="" src="https://cdn.knmi.nl/assets/forecast/2-druppels-regen-dag-01dce4fbd163e0f2ec5b3cfd234895a96595ea7a4dcf9c0d04917ec11874037e.svg" width="60px"/></td>
-                      </tr>
-                      <tr>
-                        <td>Max.</td>
-                        <td>Max.</td>
-                        <td>Max.</td>
-                      </tr>
-                      <tr>
-                        <td>8/9°</td>
-                        <td>8/10°</td>
-                        <td>8/11°</td>
-                      </tr>
-                      <tr>
-                        <td>Min.</td>
-                        <td>Min.</td>
-                        <td>Min.</td>
-                      </tr>                      
-                      <tr>
-                        <td>2/3°</td>
-                        <td>1/2°</td>
-                        <td>1/5°</td>
-                      </tr>
-                      <tr>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
-                        <td>Neerslag</td>
-                      </tr>                      
-                      <tr>
-                        <td>0/3mm</td>
-                        <td>0/5mm</td>
-                        <td>0/5mm</td>
-                      </tr>
-                      <tr>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
-                        <td>Neerslagkans</td>
-                      </tr>                                            
-                      <tr>
-                        <td>50%</td>
-                        <td>60%</td>
-                        <td>60%</td>
-                      </tr>
-                      <tr>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
-                        <td>Zonneschijn</td>
-                      </tr>                      
-                      <tr>
-                        <td>20%</td>
-                        <td>30%</td>
-                        <td>30%</td>
-                      </tr>
-                      <tr>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
-                        <td>Windkracht</td>
-                      </tr>
-                      <tr>
-                        <td>ZW 4</td>
-                        <td>Z 4</td>
-                        <td>NO 3</td>
-                      </tr>
-
-                    </table>
-                    <p>""".trimIndent() + getString(R.string.menu_knmi_text_source) + """</p>
-                </body>
-            </html>
-        """.trimIndent(),
-            "text/html", "UTF-8"
-        )
         RetrieveWebPage().execute(getURL())
     }
 
@@ -338,8 +118,25 @@ class KnmiSixDayForecastFragment : Fragment() {
                 return
             }
 
-            val tableData = Array<Array<String>>(6, { Array<String>(15, {""}) })
-            tableWrapperElement.select("li").forEachIndexed { colIndex, column ->
+            val tableData = getTableData(tableWrapperElement)
+            val columnsPerRow = CalculateColumnsPerRow(webView)
+            val htmlTable = getHtmlTable(tableData, columnsPerRow)
+            val htmlPageToShow = getHtmlPageWithTable(htmlTable)
+            webView.loadData(htmlPageToShow, "text/html", "UTF-8")
+        }
+
+        private fun getHtmlTable(tableData: Array<Array<String>>, columnsPerRow: Int) : String {
+            val numberOfRows = ceil((tableData.size / columnsPerRow).toDouble())
+            for (i in tableData.indices) {
+
+            }
+        }
+
+        // Get the weather data per day of the week
+        private fun getTableData(tableWrapperElement: Element) : Array<Array<String>> {
+            val weatherPerDay = tableWrapperElement.select("li")
+            val tableData = Array<Array<String>>(weatherPerDay.size, { Array<String>(15, {""}) })
+            weatherPerDay.forEachIndexed { colIndex, column ->
                 var rowIndex = 0
 
                 val dayOfTheWeek = column.selectFirst("strong.weather-map__table-cell")
@@ -365,6 +162,51 @@ class KnmiSixDayForecastFragment : Fragment() {
                     }
                 }
             }
+
+            return tableData
+        }
+
+        private fun getHtmlPageWithTable(table: String) : String {
+            val webView = binding.webView
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(webView.context)
+            val darkMode = sharedPreferences.getString("dark_mode", "dark_mode_no")
+            var backgroundColor = "white"
+            var textColor = "black"
+            if (darkMode == "dark_mode_yes") {
+                backgroundColor = "rgb(48, 48, 48)" // Android dark mode color
+                textColor = "rgb(193, 193, 193)" // Android dark mode color
+            }
+
+            return """<html>
+                <head>
+                    <style type='text/css'>
+                        body {
+                          font-size: 14px;
+                          color:""".trimIndent() + textColor + """;
+                          background-color:""".trimIndent() + backgroundColor + """;
+                        }
+                        tr:nth-child(2n+3) {
+                          height: 30px;
+                          vertical-align: text-top;
+                          color:""".trimIndent() + textColor + """;
+                        }
+                        tr:nth-child(1) {
+                          font-weight: bold;
+                          color:""".trimIndent() + textColor + """;                          
+                        }
+                        tr:nth-child(19) {
+                          font-weight: bold;
+                          color:""".trimIndent() + textColor + """;                          
+                        }
+                        tr:nth-child(2n) {
+                          color: grey;
+                        }
+                        tr {
+                          font-size: 14px;
+                        }
+                    </style>
+                </head>
+                <body>\n""" + table + """</body>"""
         }
     }
 }
